efc0f7270cb305d286ca9d4c4f2094ef8ec6755d
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-03-09 13:38:43 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,17 @@ module.exports = {
     interpreter: "python3",
     autorestart: true,
     watch: false,
-    max_memory_restart: "300M",
+    max_memory_restart: "150M",
+    restart_delay: 10000,
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    error_file: "logs/naoris-error.log",
+    out_file: "logs/naoris-out.log",
+    merge_logs: true,
+    max_size: "10M",
+    rotate_logs: true,
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      TZ: "Asia/Jakarta"
     }
   }]
 }
